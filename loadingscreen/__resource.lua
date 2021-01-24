2462ecc2ef3625f8af58f3736f082fce2d6b1455
tygovanist <66074344+tygovanist@users.noreply.github.com>
2021-01-24 14:02:25 +0100

    Add files via upload

---
+++ loadingscreen/__resource.lua
@@ -0,0 +1,19 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+
+
+files {
+
+    'index.html',
+
+    'style.css',
+
+    'background.jpg',
+
+    'music.mp3'
+
+}
+
+
+
+loadscreen 'index.html'
